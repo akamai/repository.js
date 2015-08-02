e36f0b36737550b4cf10ef433185414b08133561
Matt Solnit <msolnit@soasta.com>
2015-08-01 21:58:34 -0700

    Re-factor HTTP request code into a separate file for use by other API wrappers.

--- Tokens.js
+++ Tokens.js
@@ -1,7 +1,5 @@
 var debug_log = require("./debug_log.js");
-
-var url = require("url");
-var http = require("http");
+var api_request = require("./api_request.js");
 
 function Tokens(service_url) {
 	this.endpoint = service_url + "/Tokens";
@@ -19,7 +17,7 @@ Tokens.prototype.connect = function(tenant_name, user_name, password, callback) 
 		credentials.tenant = tenant_name;
 	}
 
-	sendRequest(null, this.endpoint, "PUT", credentials, function(error, responseBody) {
+	api_request(null, this.endpoint, "PUT", credentials, function(error, responseBody) {
 		if (error) {
 			// Request failed.
 			callback(error, null);
@@ -34,74 +32,7 @@ Tokens.prototype.connect = function(tenant_name, user_name, password, callback) 
 Tokens.prototype.disconnect = function(token, callback) {
 	debug_log("disconnect()");
 
-	sendRequest(null, this.endpoint + "/" + token, "DELETE", null, callback);
+	api_request(null, this.endpoint + "/" + token, "DELETE", null, callback);
 };
-
-function sendRequest(token, endpoint, method, requestBody, callback) {
-	debug_log("sendRequest(endpoint=" + endpoint + ", method=" + method + ", requestBody=" + requestBody + ")");
-
-	if (typeof requestBody === "object") {
-		requestBody = JSON.stringify(requestBody);
-		debug_log("Serialized requestBody to: " + requestBody);
-	}
-
-	var headers = {
-		"Connection": "close"
-	};
-
-	if (requestBody) {
-		headers["Content-Type"] = "application/json";
-		headers["Content-Length"] = requestBody.length;
-	}
-
-	if (token) {
-		headers["X-Auth-Token"] = token;
-	}
-
-	var options = url.parse(endpoint);
-	options.method = method;
-	options.headers = headers;
-
-	var responseBody = "";
-
-	var req = http.request(options, function(response) {
-		response.on('data', function (chunk) {
-			responseBody += chunk;
-		});
-
-		response.on('end', function () {
-			var responseCode = response.statusCode;
-			debug_log("Got response code: " + responseCode + ", response body: " + responseBody);
-
-			if (callback) {
-				if (responseBody.trim().length == 0) {
-					responseBody = null;
-				} else {
-					responseBody = JSON.parse(responseBody);
-				}
-
-				// Consider anything in the 200-299 range to be success.
-				var error = null;
-				if (responseCode < 200 || responseCode >= 300) {
-					// Request failed.
-					error = {
-						code: responseCode,
-						message: response.statusMessage
-					};
-				}
-
-				callback(error, responseBody);
-			}
-		});
-
-		response.on('error', console.log);
-	});
-
-	req.on('error', function(e) {
-		callback(e, null);
-	});
-
-	req.end(requestBody);
-}
 
 module.exports = Tokens;
