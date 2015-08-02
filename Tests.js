771efffc1d49210ee69ef00c23c1bacd21ec462b
Matt Solnit <msolnit@soasta.com>
2015-08-01 21:31:30 -0700

    Q testing continues.

--- Tests.js
+++ Tests.js
@@ -8,37 +8,42 @@ var password = "";
 var repo = new SOASTA.Repository("http://localhost:8080/concerto/services/rest/RepositoryService/v1");
 
 var connect = Q.denodeify(repo.connect);
+var disconnect = Q.denodeify(repo.disconnect);
+var createObject = Q.denodeify(repo.createObject);
+var getObjectByID = Q.denodeify(repo.getObjectByID);
+var queryObjects = Q.denodeify(repo.queryObjects);
+var updateObject = Q.denodeify(repo.updateObject);
 
 connect(tenantName, userName, password).then(function() {
 	console.log("Got connect callback!");
+
+	return createObject({
+		name: "some name!",
+		type: "some type!"
+	});
 }, function(error) {
 	console.log("Connect failed! " + error.message);
+}).then(function() {
+	console.log("Got createObject callback!");
+	return getObjectByID("composition", 42);
+}).then(function() {
+	console.log("Got getObjectByID callback!");
+	return queryObjects("composition", {
+		"last_played": "whenever!"
+	});
+}).then(function() {
+	console.log("Got queryObjects callback!");
+	return updateObject("messageclip", 21, {
+		"something!": "whatever!"
+	});
+}).then(function() {
+	console.log("Got updateObject callback!");
+	return disconnect();
+}).then(function() {
+	console.log("Got disconnect callback!");
 });
 
 /*
-repo.createObject({
-	name: "some name!",
-	type: "some type!"
-}, function() {
-	console.log("Got createObject callback!");
-});
-
-repo.getObjectByID("composition", 42, function() {
-	console.log("Got getObjectByID callback!");
-});
-
-repo.queryObjects("composition", {
-	"last_played": "whenever!"
-}, function() {
-	console.log("Got queryObjects callback!");
-});
-
-repo.updateObject("messageclip", 21, {
-	"something!": "whatever!"
-}, function() {
-	console.log("Got updateObject callback!");
-});
-
 repo.deleteObject("target", 84, function() {
 	console.log("Got deleteObject callback!");
 });
