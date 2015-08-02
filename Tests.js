5390bb7d743b4266aa1d3e0096c203c3e7a2729b
Matt Solnit <msolnit@soasta.com>
2015-08-01 22:40:59 -0700

    Tests now use real objects, with a bit of promise re-structuring to preserve scope of callback data.

--- Tests.js
+++ Tests.js
@@ -12,39 +12,50 @@ repo.connect(tenantName, userName, password).then(function() {
 	console.log("Got connect callback!");
 
 	repo.createObject({
-		name: "some name!",
-		type: "some type!"
+		type: "preference",
+		name: "some pref!"
+	}).then(function(id) {
+		console.log("Got createObject callback! id=" + id);
+
+		repo.getObjectByID("preference", id).then(function(object) {
+			console.log("Got getObjectByID callback! object=" + object);
+			return repo.queryObjects("preference", {
+				"name": "some pref!"
+			});
+		}).then(function(objectSet) {
+			console.log("Got queryObjects callback! objectSet=" + objectSet);
+			return repo.updateObject("preference", id, {
+				"description": "whatever!"
+			});
+		}).then(function() {
+			console.log("Got updateObject callback!");
+			return repo.deleteObject("preference", id);
+		}).then(function() {
+			console.log("Get deleteObject callback!");
+			return repo.readSeedData(121);
+		}).then(function() {
+			console.log("Get readSeedData callback!");
+			return repo.appendSeedData(363, "new CSV!");
+		}).then(function() {
+			console.log("Get appendSeedData callback!");
+			return repo.truncateSeedData(242);
+		}).then(function() {
+			console.log("Get truncateSeedData callback!");
+		}).done();
+	}).catch(function(error) {
+		console.log("ERROR! " + error.message);
+
+	// No matter what I do, I can't seem to make this execute only *after* the above finishes.
+	// It always happens in the middle of the above promise chain.  Something I'm not understanding
+	// about how Q or Node works, or maybe a bug in one of the libraries.
+	/*
 	}).then(function() {
-		console.log("Got createObject callback!");
-		return repo.getObjectByID("composition", 42);
-	}).then(function() {
-		console.log("Got getObjectByID callback!");
-		return repo.queryObjects("composition", {
-			"last_played": "whenever!"
-		});
-	}).then(function() {
-		console.log("Got queryObjects callback!");
-		return repo.updateObject("messageclip", 21, {
-			"something!": "whatever!"
-		});
-	}).then(function() {
-		console.log("Got updateObject callback!");
-		return repo.deleteObject("target", 84);
-	}).then(function() {
-		console.log("Get deleteObject callback!");
-		return repo.readSeedData(121);
-	}).then(function() {
-		console.log("Get readSeedData callback!");
-		return repo.appendSeedData(363, "new CSV!");
-	}).then(function() {
-		console.log("Get appendSeedData callback!");
-		return repo.truncateSeedData(242);
-	}).then(function() {
-		console.log("Get truncateSeedData callback!");
 		return repo.disconnect();
 	}).then(function() {
 		console.log("Got disconnect callback!");
-	});
+	*/
+
+	}).done();
 }, function(error) {
 	console.log("Connect failed! " + error.message);
 });
