d312a13d94e52c0338005f6aac63d3c414a00d32
Nic Jansma <nic@nicj.net>
2017-10-16 18:13:57 -0400

    Added 'indent' rule and fixed

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ exports.SOASTA = require("./lib/model/Repository");
 
 // cheap and dirty way to see if we're in a node context
 if (typeof window === "undefined") {
-  /**
-   * @export cmd
-   *
-   * @desc
-   * Exports the [Repository CLI API client]{@link cmd} to access the CLI application 
-   * to interact from a shell with the Repository
-   */
-  exports.cmd = require("./cli/cmd").program;
+    /**
+    * @export cmd
+    *
+    * @desc
+    * Exports the [Repository CLI API client]{@link cmd} to access the CLI application
+    * to interact from a shell with the Repository
+    */
+    exports.cmd = require("./cli/cmd").program;
 
-  /**
-   * @export CLI
-   *
-   * @desc
-   * Exports the [CLI utility class]{@link CLI} to initialize the API for CLI use
-   */
-  exports.CLI = require("./cli/core");
+    /**
+    * @export CLI
+    *
+    * @desc
+    * Exports the [CLI utility class]{@link CLI} to initialize the API for CLI use
+    */
+    exports.CLI = require("./cli/core");
 }
