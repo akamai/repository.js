853586ad5a90145f584314c11db512957e09718b
olaStream <ola.horovits@gmail.com>
2018-07-18 12:47:22 -0700

    Update function to timeline and updata testdata (#47)
    * Add `update` functionality to the Timeline
    
    * Delete comment from needed line
    
    * Cleaning up unused function
    
    * Fix the boolean value
    
    * Adding tests for Timeline and Annotation objects. 
    Fixes tests for Objects.
    Update package version.

--- tests/model/Objects.js
+++ tests/model/Objects.js
@@ -33,7 +33,8 @@ describe("Objects Tests", function() {
             var objectsAPI = nock("http://mpulse.soasta.com")
                 .put("/concerto/services/rest/RepositoryService/v1/Objects")
                 .reply(200,  function(uri, requestBodyObject) {
-                    assert.deepEqual(properties, requestBodyObject);
+                    var requestBody = JSON.parse(requestBodyObject);
+                    assert.deepEqual(properties, requestBody);
                     return { id: expect };
                 });
 
