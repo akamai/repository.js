7b87c2119c95167ae3a564baeb183b2fec122096
Andreas Marschke <amarschk@akamai.com>
2021-04-09 15:22:42 +0200

    Migrate transport formatter to winston 3.x, prevent argv override

--- cli/cmd.js
+++ cli/cmd.js
@@ -10,39 +10,41 @@
  * @tutorial CommandLineExtending
  */
 
-var program = require("commander");
-var path = require("path");
-var fs = require("fs");
+exports.program = function() {
+    var program = require("commander");
+    var path = require("path");
+    var fs = require("fs");
 
-program
-    .version(JSON.parse(fs.readFileSync(path.join(__dirname, "..", "package.json"), "utf-8")).version)
-    .option("-u, --username <username>", "User name")
-    .option("-p, --password <password>", "Password")
-    .option("-T, --apiToken <apiToken>", "SSO API Token")
-    .option("-t, --tenant <tenant>", "Tenant name")
-    .option("-r, --repository <url>", "Repository URL")
-    .option("-j, --json", "Output as JSON")
-    .option("-o, --output <file>", "Output file")
-    .option("-v, --verbose", "Verbose debugging")
-    .option("-a, --auth <file>", "auth.json file containing credentials")
-    .option("-d, --details <boolean>", "Enable/Disable retrieving full object details")
-    .option("-i, --stdin", "Use STDIN if file is required");
+	program
+		.version(JSON.parse(fs.readFileSync(path.join(__dirname, "..", "package.json"), "utf-8")).version)
+		.option("-u, --username <username>", "User name")
+		.option("-p, --password <password>", "Password")
+		.option("-T, --apiToken <apiToken>", "SSO API Token")
+		.option("-t, --tenant <tenant>", "Tenant name")
+		.option("-r, --repository <url>", "Repository URL")
+		.option("-j, --json", "Output as JSON")
+		.option("-o, --output <file>", "Output file")
+		.option("-v, --verbose", "Verbose debugging")
+		.option("-a, --auth <file>", "auth.json file containing credentials")
+		.option("-d, --details <boolean>", "Enable/Disable retrieving full object details")
+		.option("-i, --stdin", "Use STDIN if file is required");
 
-// commands
-program.command("query <type> [<params>]")
-    .description("query for objects by type with optional parameters (comma separated)")
-    .action(require("./query.js"));
+	// commands
+	program.command("query <type> [<params>]")
+		.description("query for objects by type with optional parameters (comma separated)")
+		.action(require("./query.js"));
 
-program.command("delete <type> <id>")
-    .description("delete objects")
-    .action(require("./delete.js"));
+	program.command("delete <type> <id>")
+		.description("delete objects")
+		.action(require("./delete.js"));
 
-program.command("update <type> <id> [file]")
-    .description("Update an object in the repository based on the JSON formatted data in a file")
-    .action(require("./update.js"));
+	program.command("update <type> <id> [file]")
+		.description("Update an object in the repository based on the JSON formatted data in a file")
+		.action(require("./update.js"));
 
-program.command("create [file]")
-    .description("Create a new object based on the JSON data in <file>")
-    .action(require("./create.js"));
+	program.command("create [file]")
+		.description("Create a new object based on the JSON data in <file>")
+		.action(require("./create.js"));
 
-exports.program = program;
+	return program;
+}
